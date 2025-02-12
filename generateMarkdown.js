65398b4f9a1837d5749309ba30d13bd5e3a17f2b
Madeline Funk <madeline.funk91@gmail.com>
2025-02-12 14:42:11 -0600

    Uploading necessary code

---
+++ generateMarkdown.js
@@ -0,0 +1,69 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+import inquirer from 'inquirer';
+
+function renderLicenseBadge(license) {
+  if (license || license === 'None') {
+    return ''; 
+  }
+
+  const badge = {
+    'MIT': '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+    'Apache 2.0':'[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+    'GPL 3.0':'[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+    'BSD 3':'[![License](https://img.shields.io/badge/License-BSD_3--Clause-orange.svg)](https://opensource.org/licenses/BSD-3-Clause)',
+  };
+
+  return badge[license] || '';
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (!license || license === 'None') {
+    return '';
+  }
+  return badge[license] || '';
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (!license || license === 'None') {
+    return '';
+  }
+  return `## License/nThis project is licensed under the ${license} license.`;
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  const { title, installation, usage, license, test, contributions, questions } = data;
+  const badge = renderLicenseBadge(license);
+  const licenseSection = renderLicenseSection(license);
+
+  return `# ${title}
+  ${badge}
+
+  ## Description
+  ${usage}
+
+   ## Installation
+  ${installation}
+
+   ## Usage
+  ${usage}
+
+  ${licenseSection}
+
+   ## Tests
+  ${test}
+
+   ## Contributions
+  ${contributions}
+
+   ## Questions
+  ${questions}
+`;
+}
+
+export default generateMarkdown;
---
+++ index.js
@@ -0,0 +1,93 @@
+// TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import colors from 'colors';
+import fs from 'fs';
+
+// TODO: Create an array of questions for user input
+// what the app is for, how to use app, how to install app, how to report issues and how to make contributions&mdash
+const questions = [ {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: 'How do you install this application?',
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'How do you use this application?',
+},
+{
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for this application:',
+    choices: [ 'MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None' ],
+},
+{
+    type: 'input',
+    name: 'test',
+    message: 'What are the test instructions for this application?',
+},
+{
+    type: 'input',
+    name: 'contributions',
+    message: 'How can you make contributions to this application?',
+},
+{
+    type: 'input',
+    name: 'questions',
+    message: 'Please enter email and Github address so you can be contacted if there are questions.'
+},
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error('Error', err);
+        } else {
+            console.log('README file has been successfully generated!');
+        }
+    });
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        const READMEContent = `
+# ${answers.title}
+
+## Description 
+${answers.usage}
+
+## Installation 
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License 
+${answers.license}
+
+## Tests 
+${answers.test}
+
+## Contributions 
+${answers.contributions}
+
+## Questions 
+${answers.questions}
+`;
+writeToFile('README.md', READMEContent);
+    })
+    .catch((error) => {
+        console.error('Error', error);
+    });
+}
+
+// Function call to initialize app
+init();
